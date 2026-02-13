510c5ccd83f506e54ed8f58b10255dcd538c6ab1
lsy3709 <lsy3709@naver.com>
2026-02-13 15:10:32 +0900

    6교시, 데이터 수집 예시2, 기본 수집

--- src/main/java/com/busanit501/jsp_server_project1/springex_0213_keep/controller/SampleController.java
+++ src/main/java/com/busanit501/jsp_server_project1/springex_0213_keep/controller/SampleController.java
@@ -3,6 +3,7 @@ package com.busanit501.jsp_server_project1.springex_0213_keep.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Log4j2
@@ -19,6 +20,16 @@ public class SampleController {
     @GetMapping("/ex1")
     public void ex1(String name, int age) {
         log.info("ex1 ~~~");
+        log.info("데이터 수집 : name : " + name);
+        log.info("데이터 수집 : age : " + age);
+    }
+
+    // http://localhost:8080/ex2?name=이상용&age=20
+    // http://localhost:8080/ex2
+    @GetMapping("/ex2")
+    public void ex2(@RequestParam(name = "name", defaultValue = "이름을 입력해주세요") String name,
+                    @RequestParam(name = "age", defaultValue = "나이를 입력해주세요") int age) {
+        log.info("ex2 ~~~");
         log.info("데이터 수집 : name : " + name);
         log.info("데이터 수집 : age : " + age);
     }
--- src/main/java/com/busanit501/jsp_server_project1/springex_0213_keep/controller/TodoController.java
+++ src/main/java/com/busanit501/jsp_server_project1/springex_0213_keep/controller/TodoController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-// http://localhost:8080/todo/ 관련된 업무는 내가 처리할게.
+// http://localhost:8080/todo2/ 관련된 업무는 내가 처리할게.
 @RequestMapping("/todo2")
 @Log4j2
 public class TodoController {
 
-    // http://localhost:8080/todo/list
+    // http://localhost:8080/todo2/list
     // 뷰 리졸버가 연결되어서,
-    // todo/list -> WEN-INF/views/todo/list.jsp 연결 설정됨.
+    // todo/list -> WEN-INF/views/todo2/list.jsp 연결 설정됨.
     @RequestMapping("/list")
     public void list() {
         log.info("todo list...");
